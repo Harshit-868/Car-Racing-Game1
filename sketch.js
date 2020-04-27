296cb6d1a484ca4d8adfd801de4e3ba4fec976a2
Harshit-868 <harshit.whitehat@gmail.com>
2020-04-27 20:43:41 +0530

    Added code to update position in two different tabs

--- sketch.js
+++ sketch.js
@@ -1,29 +1,45 @@
 var ball;
+var database;
+var position;
 
 function setup(){
+    database = firebase.database();
+    // console.log(database);
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    var ballRef = database.ref('ball/position');
+    ballRef.on("value", readPosition, showError);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
         changePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
+    } else if(keyDown(RIGHT_ARROW)){
         changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
+    } else if(keyDown(UP_ARROW)){
         changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
+    } else if(keyDown(DOWN_ARROW)){
         changePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+async function changePosition(x, y){
+    await database.ref('ball/position').set({
+        'x': position.x + x,
+        'y': position.y + y
+    });
+}
+
+function readPosition(data) {
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function showError() {
+    console.log("Error: Could not connect to the database.");
 }
